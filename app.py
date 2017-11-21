57fae5ec607def780eca6b8cedd66a19863d9752
Kelemete Seto <oteswang@gmail.com>
2017-11-20 17:14:25 -1000

    create class to make it OOP

--- app.py
+++ app.py
@@ -11,25 +11,42 @@ Rules of the game:
 """
 from getpass import getpass as maskinput
 
-SELECTION = [select for select in ["rock", "paper", "scissors"]]
-PLAYER_ONE_NAME = input("Player ONE, enter your name. 'q' to quit: ").title()
-PLAYER_TWO_NAME = input("Player TWO, enter your name. 'q' to quit: ").title()
-COUNTER = int(input("Enter number of games to play. 'q' to quit: "))
-
-while COUNTER != 0:
-    print("Enter rock, paper, scissors")
-    PLAYER_ONE_CHOICE = maskinput(("{}, your choice. 'q' to quit: ").format(PLAYER_ONE_NAME)).lower()
-    PLAYER_TWO_CHOICE = maskinput(("{}, your choice. 'q' to quit: ").format(PLAYER_TWO_NAME)).lower()
-
-    if PLAYER_ONE_CHOICE == 'q' or PLAYER_TWO_CHOICE == 'q':
-        print('Goodbye!')
-        break
+class JanKenPo:
     
-    if PLAYER_ONE_CHOICE == PLAYER_TWO_CHOICE:
-        print("It's a tie! Play again.")
-    
-    else:
-        print("{} chose {}".format(PLAYER_ONE_NAME, PLAYER_ONE_CHOICE))
-        print("{} chose {}".format(PLAYER_TWO_NAME, PLAYER_TWO_CHOICE))
+    def __init__(self, name, choice):
+        self.name = name        
 
-    COUNTER -= 1
+    def get_player_name(self):
+        return self.name
+
+    # def get_player_two_name(self):
+    #     return self.player_two
+
+    # SELECTION = [select for select in ["rock", "paper", "scissors"]]
+    # PLAYER1_NAME = input("Player ONE, enter your name. 'q' to quit: ").title()
+    # PLAYER2_NAME = input("Player TWO, enter your name. 'q' to quit: ").title()
+    # COUNTER = int(input("Enter number of games to play. 0 to quit: "))
+
+    # while COUNTER != 0:
+    #     print("Enter rock, paper, or scissors as choices. 'q' to quit")
+    #     PLAYER1_CHOICE = maskinput(("{}, your choice: ").format(PLAYER1_NAME)).lower()
+    #     PLAYER2_CHOICE = maskinput(("{}, your choice: ").format(PLAYER2_NAME)).lower()
+
+    #     if PLAYER1_CHOICE == 'q' or PLAYER2_CHOICE == 'q':
+    #         print('Goodbye!')
+    #         break
+        
+    #     if PLAYER1_CHOICE == PLAYER2_CHOICE:
+    #         print("It's a tie! Play again.")
+        
+    #     else:
+    #         print("{} chose {}".format(PLAYER1_NAME, PLAYER1_CHOICE))
+    #         print("{} chose {}".format(PLAYER2_NAME, PLAYER2_CHOICE))
+
+    #     COUNTER -= 1
+
+if __name__ == "__main__":
+    frank = JanKenPo("Frank", "rock")
+    tammy = JanKenPo("Tammy", "paper")
+
+    print(frank.get_player_name(), tammy.get_player_name())
